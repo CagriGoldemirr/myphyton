94edfc0c89494bd02faafb63fd2f5a9cefd6a603
Çağrı Göldemir <121401026+CagriGoldemirr@users.noreply.github.com>
2022-12-28 19:47:11 +0300

    Add files via upload

---
+++ FORlooop.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 26 18:23:08 2022
+
+@author: Asus
+"""
+#FOR
+    #benzer işlemleri belirli bir şarta göre tekrarlamak için kullamılır
+    #döngü tek tek elemanları çalıştırır
+sehirler = ["ankara","istanbul","balıkesir","izmir"]
+print(sehirler[0])
+print(sehirler[1])
+print(sehirler[2])
+print(sehirler[3])
+ #eğer 100 lerce şehir,data olsaydı tek tek yazmak zorunda mı kalacaktım?
+ #döngüler birbirine benzeyen işleri daha hızlı yapılmasının sağlar
+for sehir in sehirler:
+    print(sehir[0:1])  #sehirler iççnde bir döngü yapacağını anlıyoruz bu satırdan. oradaki sehir nereden geliyor peki? programın kontrol ettiği,uğraştığı her bir şehrin takma adı gibi düşünebiilirz
+
+for sehir in sehirler:
+    print(sehir[0:9]) # elemanların her birinin 0 da  9. harfe kadar yazdır demek oluyor bu
+for sehir in sehirler:
+    print(sehir[2:5])
+    
+    
+for sehir in sehirler:
+    print(sehir + " " + "için kod = " + sehir[0:5])    
+    
+
+      
+
+
+ 
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
